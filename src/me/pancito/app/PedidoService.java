76c8007033a3a66f5afc488b92876063c2c734ca
Benítez Tomás Gabriel <96633613+benitez-tomas@users.noreply.github.com>
2025-05-20 21:02:19 -0300

    Update PedidoService.java

--- src/me/pancito/app/PedidoService.java
+++ src/me/pancito/app/PedidoService.java
@@ -1,6 +1,7 @@
 package me.pancito.app;
 
 import me.pancito.excepciones.StockInsuficienteException;
+import me.pancito.exceptiones.ErrorDeValidacionException;
 import me.pancito.pedidos.LineaPedido;
 import me.pancito.pedidos.Pedido;
 import me.pancito.productos.Producto;
@@ -15,6 +16,7 @@ public class PedidoService {
     public PedidoService(ProductoService productoService, Scanner scanner) {
         this.productoService = productoService;
         this.scanner = scanner;
+        // Usamos el mismo scanner que en Main para no crear objetos al pedo
     }
 
     public void crearPedido() {
@@ -22,7 +24,7 @@ public class PedidoService {
         while (true) {
             productoService.listarProductos();
             System.out.print("ID del producto a agregar (o 0 para finalizar): ");
-            int id = Integer.parseInt(scanner.nextLine());
+            int id = scanner.nextInt();
             if (id == 0) break;
             Producto producto = productoService.buscarPorId(id);
             if (producto == null) {
@@ -30,17 +32,21 @@ public class PedidoService {
                 continue;
             }
             System.out.print("Cantidad: ");
-            int cantidad = Integer.parseInt(scanner.nextLine());
-            try {
+            int cantidad = scanner.nextInt();
+            try { // Chequeamos que la cantidad sea válida.
                 if (cantidad > producto.getStock()) {
                     throw new StockInsuficienteException("No hay suficiente stock.");
+                } else if (cantidad < 1) {
+                    throw new ErrorDeValidacionException("Por favor indique una cantidad mayor o igual a 1 (uno).");
                 }
                 producto.setStock(producto.getStock() - cantidad);
                 pedido.agregarLinea(new LineaPedido(producto, cantidad));
-            } catch (StockInsuficienteException e) {
+            } catch (StockInsuficienteException | ErrorDeValidacionException e) {
                 System.out.println("Error: " + e.getMessage());
             }
         }
+        // Viendo que el pedido no tenga un total igual a 0,
+        // nos aseguramos de que no haya un pedido vacío
         if (!pedido.toString().contains("Total: $0.0")) {
             pedidos.add(pedido);
             System.out.println("Pedido creado:\n" + pedido);
